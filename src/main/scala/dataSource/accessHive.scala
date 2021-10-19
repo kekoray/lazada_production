f203fdccbe34a4bca0ad412401366519d06ffb53
kekoray <61939347+kekoray@users.noreply.github.com>
2021-10-19 16:04:29 +0800

    init

--- src/main/scala/dataSource/accessHive.scala
+++ src/main/scala/dataSource/accessHive.scala
@@ -65,22 +65,22 @@ import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructFi
       .schema(schema)
       .csv("src/main/resources/student.csv")
 
-    val result: Dataset[Row] = csvDF.select("id")
+    val result: Dataset[Row] = csvDF.select("name","id","age","gpa")
     //      .where("age > 18")
 
 
     /*
-    前提要关闭动态分区hive.exec.dynamic.partition.mode=nonstrict
+    按照数据位置顺序插入数据,忽略列名,前提要允许所有字段使用动态分区 [hive.exec.dynamic.partition.mode=nonstrict]
     A. overwrite + insertInto: 只覆盖分区相同的数据
     B. append + insertInto: 表末尾追加增量数据
 
      */
 
-    //    spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
-    //    result.write
-    //      .mode("append")
-    //      //      .partitionBy("age")
-    //      .insertInto("spark_test.student")
+        // 设置允许所有字段使用动态分区
+        spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
+        result.write
+          .mode("append")
+          .insertInto("spark_test.student")
 
 
     /*
@@ -92,16 +92,20 @@ import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructFi
         1.表存在且表已有数据,直接在表末尾追加增量数据
         2.表存在且表无数据,报错并提议使用insertInto
         3.不存在,自动建表并插入数据
+
     C. error + saveAsTable:
+        1.只要表存在,就抛出异常
+        2.不存在,自动建表并插入数据
+
     D. ignore + saveAsTable:
         1.只要表存在,无论有无数据,都无任何操作
         2.表不存在,自动建表并插入数据
 
-
      */
-    result.write
-      .mode("append")
-      .saveAsTable("spark_test.student11")
+
+//    result.write
+//      .mode("error")
+//      .saveAsTable("spark_test.student2")
 
 
     spark.stop()
