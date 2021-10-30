e392d6f8a18837fdf2e6295e5d44da16ad549560
kekoray <61939347+kekoray@users.noreply.github.com>
2021-10-30 11:55:40 +0800

    init

--- src/main/scala/dataSource/accessRedis.scala
+++ src/main/scala/dataSource/accessRedis.scala
@@ -31,68 +31,74 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
     import spark.implicits._
     val sc: SparkContext = spark.sparkContext
+
+    //
     import com.redislabs.provider.redis._
 
-    // 获取相匹配的key
-    sc.fromRedisKeys(Array("abc", "customer_name"), 3).collect().foreach(println) // ??不懂
 
-    // 获取匹配规则的key
+    // ---------------------  读写操作  ------------------------------
+    // KV处理的是string类型数据
+
+    // 获取相匹配的key
+    sc.fromRedisKeys (Array("customer*"), 3).collect().foreach(println)//???
+
+
+
     sc.fromRedisKeyPattern("customer*", 5).collect().foreach(println)
 
-    // 返回kv值
+    // 根据key获取对应的value值
     sc.fromRedisKV("customer_gender").collect().foreach(println)
     sc.fromRedisKV("customer*").collect().foreach(println)
-
     sc.fromRedisList("customer_phone").collect().foreach(println)
     sc.fromRedisHash("gps").collect().foreach(println)
     sc.fromRedisHash("user:1000").collect().foreach(println)
     sc.fromRedisSet("user:1000:*").collect().foreach(println)
     sc.fromRedisSet(Array("user:1000:email", "user:1000:phones")).collect().foreach(println)
 
+    // ---------------------  写入操作  ------------------------------
+    sc.toRedisLIST(Seq("lili", "or", "zz").toDS().rdd, "list_name")
+    sc.toRedisHASH(Seq(("1", "lili"), ("2", "mimi")).toDS().rdd, "hash_name")
 
-    import com.redislabs.provider.redis._
-    sc.toRedisKV(Seq(("1", "kkk")).toDS().rdd)
-    sc.toRedisHASH(Seq(("1", "kkk")).toDS().rdd, "hsahKEY")
 
+    // ---------------------  hash表操作  ------------------------------
     val df: DataFrame = Seq(("John", 30), ("Peter", 45)).toDF("name", "age")
+
+    // sql形式写入,但是不能select读取
+    spark.sql(
+      """CREATE TEMPORARY VIEW person2
+        |(name STRING, age INT)
+        |USING org.apache.spark.sql.redis
+        |OPTIONS (table 'person2', key.column 'name')""".stripMargin)
+    spark.sql("""INSERT INTO TABLE person2 VALUES ('John', 63),('Peter', 65)""")
+
+    // hash表写入
     df.write
       .format("org.apache.spark.sql.redis")
       .mode("overwrite")
-      .option("table", "person") // 一级key名 [ person:随机数 ==> (name,John) (age,30) ]
-      .option("key.column", "name") // 二级key名  [ person:John ==> (age,30) ]
+      .option("table", "person") // 指定一级key名 [ person:随机数 ==> (name,John) (age,30) ]
+      .option("key.column", "name") // 指定二级key名  [ person:John ==> (age,30) ]
       .save()
+    /*127.0.0.1:6379[1]> keys 'person*'
+            1) "person:Peter"
+            2) "person:John"
+      127.0.0.1:6379[1]> hgetall 'person:Peter'
+            1) "age"
+            2) "45"                 */
 
-    // sql形式
-    val ddlView = "CREATE TEMPORARY VIEW person2 (name STRING, age INT) USING org.apache.spark.sql.redis OPTIONS (table 'person2', key.column 'name')"
-    spark.sql(ddlView)
-    val queryView = "INSERT INTO TABLE person2 VALUES ('John', 63),('Peter', 65)"
-    spark.sql(queryView)
+    // hash表读取
+    spark.read
+      .format("org.apache.spark.sql.redis")
+      .option("table", "person")
+      .option("key.column", "name")
+      .load()
+      .show()
+    /*+-----+---+
+      | name|age|
+      +-----+---+
+      |Peter| 45|
+      | John| 30|
+      +-----+---+*/
 
-
-
-
-    //    val redisRDD: RDD[(String, String)] = sc.fromRedisKV("*") // 获取string类型数据
-    //    print(redisRDD.collect())
-
-    //    spark.read
-    //      .format("org.apache.spark.sql.redis")
-    //      .option("table", "person")
-    //      .option("key.column", "name")
-    //      .load()
-    //      .show()
-
-    // 写入操作
-    //    val data: RDD[(String, String)] = sc.makeRDD(Seq(("1", "lili"), ("2", "mimi")))
-    //    sc.toRedisLIST(sc.makeRDD(Seq("lili","or","zz")), "list_name")
-    //    sc.toRedisHASH(data,"hash_name")
-
-    // 写入
-    //    data.toDF("id","name").write
-    //      .format("org.apache.spark.sql.redis")
-    //      .option("table", "hash_name")
-    //      .option("key.column", "name")
-    //      .mode("Overwrite")
-    //      .save()
 
 
     spark.stop()
