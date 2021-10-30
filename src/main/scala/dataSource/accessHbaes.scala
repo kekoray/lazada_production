518233e530507dfbef1cd49e59784ce422638bc0
kekoray <61939347+kekoray@users.noreply.github.com>
2021-10-30 17:28:52 +0800

    init

--- src/main/scala/dataSource/accessHbaes.scala
+++ src/main/scala/dataSource/accessHbaes.scala
@@ -1,5 +1,8 @@
 package dataSource
 
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 
@@ -20,8 +23,26 @@ import org.apache.spark.sql.SparkSession
       val spark = SparkSession.builder().appName("accessHbaes").master("local[*]").getOrCreate()
       val sc: SparkContext = spark.sparkContext
 
-      HBaseConfiguration.create()
+      val conf = HBaseConfiguration.create()
+      conf.set("hbase.zookeeper.quorum","cdh1")
+      conf.set("hbase.zookeeper.property.clientPort","2181")
+      conf.set(TableInputFormat.INPUT_TABLE,"default")
 
+      val hBaseRDD = sc.newAPIHadoopRDD(conf,classOf[TableInputFormat],
+        classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
+        classOf[org.apache.hadoop.hbase.client.Result]).cache()
+
+      // RDD数据操作
+      val data = hBaseRDD.map(x => {
+        val result = x._2
+        val key = Bytes.toString(result.getRow)
+        val value = Bytes.toString(result.getValue("info".getBytes,"click_count".getBytes))
+        (key,value)
+      })
+
+      data.foreach(println)
+
+      sc.stop()
     }
   }
 }
