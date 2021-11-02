921392ed920566e5a1c5dd72fed5c048e8c136db
kekoray <61939347+kekoray@users.noreply.github.com>
2021-11-02 20:03:58 +0800

    init

--- src/main/scala/dataSource/accessHbaes.scala
+++ src/main/scala/dataSource/accessHbaes.scala
@@ -3,12 +3,16 @@ package dataSource
 import java.util.UUID
 
 import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
-import org.apache.hadoop.hbase.client.{ConnectionFactory, Put}
-import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.client.{ColumnFamilyDescriptorBuilder, ConnectionFactory, Put, Result, TableDescriptorBuilder}
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.mapreduce.Job
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Column, DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.execution.datasources.hbase.HBaseTableCatalog
 
 import scala.util.Try
 
@@ -28,51 +32,126 @@ import scala.util.Try
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 
-    val spark = SparkSession.builder().appName("accessHbaes").master("local[*]").getOrCreate()
+    val spark = SparkSession.builder().appName("accessHbaes").master("local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
     val sc: SparkContext = spark.sparkContext
     import spark.implicits._
 
     // hbase的配置
-    val conf = HBaseConfiguration.create()
-    conf.set("hbase.zookeeper.quorum", "192.168.100.216")
-    conf.set("hbase.zookeeper.property.clientPort", "2181")
-    conf.set("hbase.defaults.for.version.skip", "true")
+    val hbaseConf = HBaseConfiguration.create()
+    hbaseConf.set("hbase.zookeeper.quorum", "192.168.100.216")
+    hbaseConf.set("hbase.zookeeper.property.clientPort", "2181")
+    //在IDE中设置此项为true，避免出现"hbase-default.xml"版本不匹配的运行时异常
+    hbaseConf.set("hbase.defaults.for.version.skip", "true")
 
-    conf.set(TableInputFormat.INPUT_TABLE, "person")
+    // 获取admina管理员
+    //    val hbaseConn = ConnectionFactory.createConnection(hbaseConf)
+    //    val admin = hbaseConn.getAdmin
 
-
-    val hbaseConn = ConnectionFactory.createConnection(conf)
-    val admin = hbaseConn.getAdmin
-    println(admin.listTableNames().toList)
-
-
-    val hBaseRDD = spark.sparkContext.newAPIHadoopRDD(conf,
-      classOf[TableInputFormat],
-      classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
-      classOf[org.apache.hadoop.hbase.client.Result]).cache()
-    println(hBaseRDD.count())
+    // 删除表
+    //    admin.deleteTable(TableName.valueOf("table_name"))
 
 
-    println(hBaseRDD.map(_._2).count())
+    /*
+    TableInputFormat/TableOutputFormat是org.apache.hadoop.hbase.mapreduce包下
+     */
+    // =======================  读取数据  =======================
 
-    import spark.implicits._
+    // 设置查询的表名
+    //    hbaseConf.set(TableInputFormat.INPUT_TABLE, "person")
+    //    val hBaseRDD: RDD[(ImmutableBytesWritable, Result)] = spark.sparkContext.newAPIHadoopRDD(hbaseConf,
+    //      classOf[TableInputFormat],
+    //      classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
+    //      classOf[org.apache.hadoop.hbase.client.Result]).cache()
+    //
+    //    // 遍历结果
+    //    hBaseRDD.map({ case (_, result) =>
+    //      // 获取行键
+    //      val key = Bytes.toString(result.getRow)
+    //      // 通过列族和列名获取值
+    //      val name = Bytes.toString(result.getValue("name".getBytes, "china".getBytes))
+    //      (key, name)
+    //    }).toDF("id", "name").show(false)
+    //
+    //
+
+    //
+    //    // =======================  写取数据  =======================
+    //
+    //    // 判断表是否存在,不存在则创建表
+    //    if (!admin.tableExists(TableName.valueOf("person2"))) {
+    //      val table_desc = TableDescriptorBuilder.newBuilder(TableName.valueOf("person2"))
+    //      //指定列簇,不需要创建列
+    //      val col_desc = ColumnFamilyDescriptorBuilder.newBuilder("info".getBytes()).build()
+    //      table_desc.setColumnFamily(col_desc)
+    //      admin.createTable(table_desc.build())
+    //    }
+    //
+    //    // 设置写入的表名
+    //    hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, "person2")
+    //    val job = Job.getInstance(hbaseConf)
+    //    job.setOutputKeyClass(classOf[ImmutableBytesWritable])
+    //    job.setOutputValueClass(classOf[Result])
+    //    job.setOutputFormatClass(classOf[TableOutputFormat[ImmutableBytesWritable]])
+    //
+    //
+    //    val data = sc.makeRDD(Array("1,Jack,M,26", "2,Rose,M,17")).map(_.split(","))
+    //    val dataRDD = data.map(arr => {
+    //      // 设置行健
+    //      val put = new Put(Bytes.toBytes(arr(0)))
+    //      // 设置列族和列名和值
+    //      put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("name"), Bytes.toBytes(arr(1)))
+    //      put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("gender"), Bytes.toBytes(arr(2)))
+    //      put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("age"), Bytes.toBytes(arr(3).toInt))
+    //      (new ImmutableBytesWritable, put)
+    //    })
+    //
+    //    // 数据写入hbase
+    //    dataRDD.saveAsNewAPIHadoopDataset(job.getConfiguration)
 
 
-    hBaseRDD.map({ case (_, result) =>
-      val key = Bytes.toString(result.getRow)
-      val name = Bytes.toString(result.getValue("name".getBytes, "china".getBytes))
-      (key, name)
-    }).toDF("id", "name").show(false)
+    // =======================  SparkSQL操作HBase  =======================
+    /*
+    jar包自己匹配加到maven中
+    <!-- https://mvnrepository.com/artifact/com.hortonworks/shc-core -->
+    <dependency>
+        <groupId>com.hortonworks</groupId>
+        <artifactId>shc-core</artifactId>
+        <version>1.1.0-2.1-s_2.11</version>
+    </dependency>
+
+     */
+    import spark.sql
+    // rowkey必须定义为列(col0),该列具有特定的cf(rowkey)
+    val catalog =
+      s"""{
+         |    "table":{"namespace":"default", "name":"person3"},
+         |    "rowkey":"id",
+         |    "columns":{
+         |    "id":{"cf":"rowkey", "col":"id", "type":"int"},
+         |    "name":{"cf":"info", "col":"name", "type":"string"},
+         |    "biaoqian":{"cf":"info", "col":"biaoqian", "type":"string"},
+         |    "age":{"cf":"info",  "col":"age", "type":"int"}
+         |    }
+         |    }""".stripMargin
+
+    val dataDF = sc.makeRDD(Seq(("1", "Jack", "M", "26"), ("2", "Rose", "M", "17"))).toDF()
+    dataDF.show()
+    dataDF.write.options(
+      Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "4"))
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .mode("overwrite")
+      .save()
+
+//    val read = spark.read
+    //      .options(Map(HBaseTableCatalog.tableCatalog -> catalog))
+    //      .format("org.apache.spark.sql.execution.datasources.hbase")
+    //      .load()
+    //    read.createOrReplaceTempView("table")
+    //    sql("select * from read").show()
 
 
-    // =======================  通过HTable中put方法  =======================
-    // =======================  通过TableOutputFormat向HBase写数据  =======================
-    // =======================  通过bulkload向HBase写数据  =======================
-
-
-    /*使用 saveAsHadoopDataset 写入数据
-      使用 newAPIHadoopRDD 读取数据
-      Spark DataFrame 通过 Phoenix 读写 HBase*/
     sc.stop()
 
 
--- src/main/scala/dataSource/accessHive.scala
+++ src/main/scala/dataSource/accessHive.scala
@@ -33,26 +33,26 @@ import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructFi
     import org.apache.spark.sql.functions._
     import spark.implicits._
 
-
-    //======================================================================================
-    //===========================   创建操作   =============================================
-    //======================================================================================
-    spark.sql("USE spark_test")
-    val createSql =
-      """Create External Table If Not Exists student
-        |( name String,
-        |  age  Int,
-        |  gpa  Decimal(5,2)
-        |) Comment '学生表'
-        |  Partitioned By (
-        |    dt String Comment '日期分区字段{"format":"yyyy-MM-dd"}')
-        |  Row Format Delimited
-        |    Fields Terminated By '\t'
-        |    Lines Terminated By '\n'
-        |  Stored As textfile
-        |  Location '/dataset/hive'
-        |  Tblproperties ("orc.compress" = "SNAPPY")""".stripMargin
-    spark.sql(createSql)
+//
+//    //======================================================================================
+//    //===========================   创建操作   =============================================
+//    //======================================================================================
+//    spark.sql("USE spark_test")
+//    val createSql =
+//      """Create External Table If Not Exists student
+//        |( name String,
+//        |  age  Int,
+//        |  gpa  Decimal(5,2)
+//        |) Comment '学生表'
+//        |  Partitioned By (
+//        |    dt String Comment '日期分区字段{"format":"yyyy-MM-dd"}')
+//        |  Row Format Delimited
+//        |    Fields Terminated By '\t'
+//        |    Lines Terminated By '\n'
+//        |  Stored As textfile
+//        |  Location '/dataset/hive'
+//        |  Tblproperties ("orc.compress" = "SNAPPY")""".stripMargin
+//    spark.sql(createSql)
 
 
     //======================================================================================
@@ -74,45 +74,45 @@ import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructFi
         A. overwrite + insertInto : 在不影响其他分区数据的情况下,只覆盖指定分区的数据;
         B. append + insertInto : 在表末尾追加增量数据
     */
-
-    // 常用操作,相当于(Inset Overwrite .. Partition ..)
-    resultDF.write
-      .mode("overwrite")
-      .insertInto("spark_test.student")
-
-
-    /* ------------------------  2.saveAsTable模式  ------------------------------
-    A. overwrite + saveAsTable:
-        1.表存在,schema字段数相同,会按照新schema字段位置,覆盖全表插入数据
-        2.表不存在,或者表存在且schema字段数不相同,则会按照新schema进行重新建表并插入数据
-    B. append + saveAsTable:
-        1.表存在且表已有数据,直接在表末尾追加增量数据
-        2.表存在且表无数据,报错并提议使用insertInto
-        3.不存在,自动建表并插入数据
-    C. error + saveAsTable:
-        1.只要表存在,就抛出异常
-        2.不存在,自动建表并插入数据
-    D. ignore + saveAsTable:
-        1.只要表存在,无论有无数据,都无任何操作
-        2.表不存在,自动建表并插入数据
-    */
-
-    resultDF.write
-      .mode("append")
-      .partitionBy("dt")
-      .saveAsTable("spark_test.student")
-
-
-    //======================================================================================
-    //===========================   查询操作   =============================================
-    //======================================================================================
-    spark.sql("select * from student").show()
-    spark.table("student").show()
-
-    import spark.sql
-    sql("select * from student").show()
-
-
+//
+//    // 常用操作,相当于(Inset Overwrite .. Partition ..)
+//    resultDF.write
+//      .mode("overwrite")
+//      .insertInto("spark_test.student")
+//
+//
+//    /* ------------------------  2.saveAsTable模式  ------------------------------
+//    A. overwrite + saveAsTable:
+//        1.表存在,schema字段数相同,会按照新schema字段位置,覆盖全表插入数据
+//        2.表不存在,或者表存在且schema字段数不相同,则会按照新schema进行重新建表并插入数据
+//    B. append + saveAsTable:
+//        1.表存在且表已有数据,直接在表末尾追加增量数据
+//        2.表存在且表无数据,报错并提议使用insertInto
+//        3.不存在,自动建表并插入数据
+//    C. error + saveAsTable:
+//        1.只要表存在,就抛出异常
+//        2.不存在,自动建表并插入数据
+//    D. ignore + saveAsTable:
+//        1.只要表存在,无论有无数据,都无任何操作
+//        2.表不存在,自动建表并插入数据
+//    */
+//
+//    resultDF.write
+//      .mode("append")
+//      .partitionBy("dt")
+//      .saveAsTable("spark_test.student")
+//
+//
+//    //======================================================================================
+//    //===========================   查询操作   =============================================
+//    //======================================================================================
+//    spark.sql("select * from student").show()
+//    spark.table("student").show()
+//
+//    import spark.sql
+//    sql("select * from student").show()
+//
+//
 
     spark.stop()
 
