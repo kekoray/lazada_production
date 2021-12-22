d0884d5641249a9b5d1d5fabca9911b505cb8df1
kekoray <61939347+kekoray@users.noreply.github.com>
2021-12-22 19:23:35 +0800

    1

--- src/main/scala/spark_ml/_0x_test.scala
+++ src/main/scala/spark_ml/_0x_test.scala
@@ -2,10 +2,11 @@ package spark_ml
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml
-import org.apache.spark.ml.feature.ChiSqSelector
+import org.apache.spark.ml.feature.{ChiSqSelector, StringIndexer, VectorAssembler}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.ml.stat.Correlation
 import org.apache.spark.ml.stat.Summarizer._
 import org.apache.spark.sql.types.{DataTypes, StructType}
 
@@ -29,6 +30,13 @@ import org.apache.spark.sql.types.{DataTypes, StructType}
     val spark = SparkSession.builder().appName("ChiSquareTestExample").master("local[*]").getOrCreate()
     val sc = spark.sparkContext
     import spark.implicits._
+
+    // ==================================  RDD转为DF的3种方式  =====================================================
+    /*
+          1.rdd + 样例类 + toDF
+          2.rdd + Row类 + schema + createDF
+          3.read + schema
+     */
 
     val path = "src/main/resources/ML_data/iris.data"
 
@@ -54,9 +62,24 @@ import org.apache.spark.sql.types.{DataTypes, StructType}
     val df3: DataFrame = spark.read.format("csv").schema(schema).load(path)
 
 
-    // corr相关系数
-    Correlation.
 
+
+    // ==================================  案例实践  =====================================================
+
+    // 1.统计DataFrame的corr相关系数
+    // 由于Correlation.corr方法中的col参数必须是vector类型,需要使用到特征组合器VectorAssembler将需要统计的多列转化为vector类型即可
+    val vectorAssembler: VectorAssembler = new VectorAssembler().setInputCols(Array("sepal_length", "sepal_width", "petal_length", "petal_width")).setOutputCol("features")
+    val featuresDF: DataFrame = vectorAssembler.transform(df3)
+    val corrDF = Correlation.corr(featuresDF, "features", "pearson")
+    corrDF.show(false)
+
+    // 2.标签列数值化操作
+    val labelDF = new StringIndexer().setInputCol("classlabel").setOutputCol("labelcol_index").fit(featuresDF).transform(featuresDF)
+    labelDF.show(100, false)
+
+    // 3.卡方验证选择特征列
+    val chisqDF: DataFrame = new ChiSqSelector().setFeaturesCol("features").setLabelCol("labelcol_index").setOutputCol("chisq_col").setNumTopFeatures(2).fit(labelDF).transform(labelDF)
+    chisqDF.show(100, false)
 
 
     spark.stop()
